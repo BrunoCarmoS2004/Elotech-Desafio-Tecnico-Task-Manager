1135a4ea5ebb13dd90b329ef582d696ac93fb4f9
BrunoCarmoS2004 <126525394+BrunoCarmoS2004@users.noreply.github.com>
2026-06-20 13:50:16 -0300

    chore: Iniciando projeto no GitHub

---
+++ taskmaneger/src/main/java/br/com/elotech/desafio/backend/taskmaneger/TaskmanegerApplication.java
@@ -0,0 +1,13 @@
+package br.com.elotech.desafio.backend.taskmaneger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanegerApplication.class, args);
+	}
+
+}
---
+++ taskmaneger/src/test/java/br/com/elotech/desafio/backend/taskmaneger/TaskmanegerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elotech.desafio.backend.taskmaneger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanegerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
